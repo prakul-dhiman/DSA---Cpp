2cac50a1ffae9a05c69cdab7da3323f58d10e9bd
Prakul Dhiman <prakul.12322554@lpu.in>
2026-01-27 02:26:11 +0530

    Add program to find the largest element in an array

---
+++ Arrays/largest_element.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {3, 7, 2, 9, 5};
+    int n = 5;
+
+    int maxVal = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+
+    cout << "Largest element = " << maxVal;
+    return 0;
+}
