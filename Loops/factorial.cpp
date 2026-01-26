50b9762d3970dc4c8518ca94cdbb259489e93736
Prakul Dhiman <prakul.12322554@lpu.in>
2026-01-27 02:25:01 +0530

    Add factorial calculation in factorial.cpp

---
+++ Loops/factorial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    int fact = 1;
+
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    cout << "Factorial = " << fact;
+    return 0;
+}
