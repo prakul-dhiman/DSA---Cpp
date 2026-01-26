4a60c06c084dfff43574799a5c0eaee049bc17db
Prakul Dhiman <prakul.12322554@lpu.in>
2026-01-27 02:30:52 +0530

    Implement function to find the largest number in an array

---
+++ largest_function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int largest(int arr[], int n) {
+    int maxVal = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxVal)
+            maxVal = arr[i];
+    }
+    return maxVal;
+}
+
+int main() {
+    int arr[] = {3, 9, 1, 7};
+    int n = 4;
+    cout << largest(arr, n);
+    return 0;
+}
