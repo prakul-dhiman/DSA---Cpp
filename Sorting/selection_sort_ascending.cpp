62dae16f4cacf7f84d8ae39e21d23a932d1e98c6
Prakul Dhiman <prakul.12322554@lpu.in>
2026-01-27 13:51:13 +0530

    Implement selection sort in ascending order
    Changed selection sort algorithm to sort in ascending order.

--- Sorting/selection_sort_ascending.cpp
+++ Sorting/selection_sort_ascending.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5, 1, 3, 2, 4};
+    int n = 5;
+
+    // Selection Sort - Descending
+    for (int i = 0; i < n - 1; i++) {
+        int maxIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[maxIndex]) {
+                maxIndex = j;
+            }
+        }
+        swap(arr[i], arr[maxIndex]);
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
 
